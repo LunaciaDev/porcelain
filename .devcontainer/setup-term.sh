96a282ebcdf20c88cdbbec841412c65737138b90
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2026-01-03 13:34:36 +0700

    Initial commit

---
+++ .devcontainer/setup-term.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+echo PS1="\"\[\033[1;35m\]\u\[\033[0m\]@\[\033[1;36m\]\h\[\033[0m\] \[\033[34m\]\w\[\033[0m\]\n└> \[\033[1;32m\]$\[\033[0m\] \"" >> ~/.bashrc
+echo alias ls="\"eza --icons -lah --sort=Name --group-directories-first --git\"" >> ~/.bashrc
---
+++ README.md
@@ -0,0 +1,4 @@
+# Fedora Minimal
+
+A docker image based on Fedora Minimal project. Has almost nothing pre-installed
+other than Git and DNF.
