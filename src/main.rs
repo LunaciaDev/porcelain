30b382368951b52cd5601435097b7c2517cbd725
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2026-01-14 03:58:28 +0000

    separate texture drawing; add rotation for rect

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ use porcelain::{
 struct AppState {
     rect_pos: [f32; 2],
     rect_size: [f32; 2],
+    rect_rot: f32,
     rect_texture: Option<TextureId>,
 }
 
 impl EventListener for AppState {
     fn update(&mut self, texture_context: &TextureContext, dt: f64) {
-        self.rect_pos[0] += (10. * dt) as f32;
+        self.rect_rot += 10. * (dt as f32);
 
         if self.rect_texture.is_none() {
             self.rect_texture = Some(texture_context.register_texture_rgb8(1, 1, &[255, 255, 255]));
@@ -23,12 +24,12 @@ impl EventListener for AppState {
     }
 
     fn draw(&self, draw_context: &mut DrawContext) {
-        draw_context.draw_rect_textured(
+        draw_context.draw_rect_ext(
             self.rect_pos[0],
             self.rect_pos[1],
             self.rect_size[0],
             self.rect_size[1],
-            self.rect_texture.unwrap(),
+            self.rect_rot,
             Color::from_rgba8(255, 255, 255, 255),
         );
     }
@@ -41,8 +42,9 @@ fn main() {
     };
 
     let app_state = AppState {
-        rect_pos: [0., 0.],
-        rect_size: [600., 600.],
+        rect_pos: [200., 200.],
+        rect_size: [200., 200.],
+        rect_rot: 0.,
         rect_texture: None,
     };
 
--- src/render.rs
+++ src/render.rs
@@ -5,6 +5,7 @@ use crate::{
     shader::{self, Uniforms},
     texture::TextureContext,
 };
+use glam::{Affine2, Vec2};
 use miniquad::{
     Bindings, BufferLayout, EventHandler, Pipeline, PipelineParams, RenderingBackend, TextureId,
     UniformsSource, VertexAttribute, window,
@@ -20,6 +21,12 @@ pub struct Vertex {
     pos: [f32; 2],
     color: [f32; 4],
     tex_coord: [f32; 2],
+}
+
+pub struct TextureArea {
+    pub texture: TextureId,
+    pub location: Vec2,
+    pub size: Vec2,
 }
 
 impl Vertex {
@@ -85,34 +92,7 @@ impl DrawContext {
         }
     }
 
-    pub fn clear(&mut self) {
-        self.vertex_buffer.clear();
-        self.index_buffer.clear();
-        self.draw_call_vec.clear();
-    }
-
-    pub fn draw_rect(&mut self, x: f32, y: f32, w: f32, h: f32, color: Color) {
-        self.draw_rect_textured(x, y, w, h, self.default_texture, color);
-    }
-
-    pub fn draw_rect_textured(
-        &mut self,
-        x: f32,
-        y: f32,
-        w: f32,
-        h: f32,
-        texture: TextureId,
-        color: Color,
-    ) {
-        #[rustfmt::skip]
-        let vertices = [
-            Vertex::new(    x,     y, 0., 0., color),
-            Vertex::new(    x, y + h, 0., 0., color),
-            Vertex::new(x + w,     y, 0., 0., color),
-            Vertex::new(x + w, y + h, 0., 0., color)
-        ];
-        let mut indices: [u16; 6] = [0, 1, 3, 0, 3, 2];
-
+    fn create_draw_call(&mut self, vertices: Box<[Vertex]>, indices: &[u16], texture: TextureId) {
         match self.draw_call_vec.last() {
             Some(draw_call) => {
                 if draw_call.texture != texture {
@@ -125,15 +105,78 @@ impl DrawContext {
         let current_draw_call = self
             .draw_call_vec
             .last_mut()
-            .expect("A draw call is created before if empty");
+            .expect("A draw call must have been created before");
         current_draw_call.index_indices_slice.length += indices.len();
-        for index in indices.iter_mut() {
-            // vertex_indices length should be clamped shorter than u16
-            *index += current_draw_call.vertex_indices_slice.length as u16;
-        }
-        self.index_buffer.extend(indices);
+        self.index_buffer.extend(
+            indices
+                .iter()
+                .map(|index| index + current_draw_call.vertex_indices_slice.length as u16),
+        );
         current_draw_call.vertex_indices_slice.length += vertices.len();
         self.vertex_buffer.extend(vertices);
+    }
+
+    pub fn clear(&mut self) {
+        self.vertex_buffer.clear();
+        self.index_buffer.clear();
+        self.draw_call_vec.clear();
+    }
+
+    pub fn draw_rect(&mut self, x: f32, y: f32, w: f32, h: f32, color: Color) {
+        #[rustfmt::skip]
+        let vertices = Box::new([
+            Vertex::new(    x,     y, 0., 0., color),
+            Vertex::new(    x, y + h, 0., 0., color),
+            Vertex::new(x + w,     y, 0., 0., color),
+            Vertex::new(x + w, y + h, 0., 0., color)
+        ]);
+        let indices: [u16; 6] = [0, 1, 3, 0, 3, 2];
+
+        self.create_draw_call(vertices, &indices, self.default_texture);
+    }
+
+    // Rotation in degrees
+    pub fn draw_rect_ext(&mut self, x: f32, y: f32, w: f32, h: f32, rotation: f32, color: Color) {
+        let transform_matrix =
+            Affine2::from_angle_translation(rotation.to_radians(), Vec2::new(x, y));
+        #[rustfmt::skip]
+        let vertices = [
+            Vec2::new(-w/2., -h/2.),
+            Vec2::new(-w/2.,  h/2.),
+            Vec2::new( w/2., -h/2.),
+            Vec2::new( w/2.,  h/2.),
+        ]
+        .iter()
+        .map(|point| {
+            let transformed_point = transform_matrix.transform_point2(*point);
+            Vertex::new(transformed_point.x, transformed_point.y, 0., 0., color)
+        })
+        .collect::<Vec<Vertex>>()
+        .into_boxed_slice();
+        let indices: [u16; 6] = [0, 1, 3, 0, 3, 2];
+
+        self.create_draw_call(vertices, &indices, self.default_texture);
+    }
+
+    pub fn draw_texture(
+        &mut self,
+        x: f32,
+        y: f32,
+        w: f32,
+        h: f32,
+        texture: TextureArea,
+        tint: Color,
+    ) {
+        #[rustfmt::skip]
+        let vertices = Box::new([
+            Vertex::new(    x,     y,                  texture.location.x,                  texture.location.y, tint),
+            Vertex::new(    x, y + h,                  texture.location.x, texture.location.y + texture.size.y, tint),
+            Vertex::new(x + w,     y, texture.location.x + texture.size.x,                  texture.location.y, tint),
+            Vertex::new(x + w, y + h, texture.location.x + texture.size.x, texture.location.y + texture.size.y, tint)
+        ]);
+        let indices: [u16; 6] = [0, 1, 3, 0, 3, 2];
+
+        self.create_draw_call(vertices, &indices, texture.texture);
     }
 }
 
